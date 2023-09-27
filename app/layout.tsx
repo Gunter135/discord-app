3644388c211a613c82ab0e0234f5b2becb117981
Gunter135 <evdokimov0711@mail.ru>
2023-09-27 17:40:48 +0300

    Initial Commit
    it took longer that i estimated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { ModalProvider } from '@/components/providers/modal-provider'
+import { ClerkProvider } from '@clerk/nextjs'
+import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +21,34 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+        <html lang="en" suppressHydrationWarning>
+            <body className={cn(
+                font.className,
+                "bg-white dark:bg-[#313338]"
+                )}>
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='dark'
+                    enableSystem={false}
+                    storageKey='ds-theme'>
+                <SocketProvider>
+                    <ModalProvider/>
+                    <QueryProvider>
+                        {children}
+                    </QueryProvider>
+                </SocketProvider>
+                </ThemeProvider>
+            </body>
+        </html>
+    </ClerkProvider>
   )
 }
+
+{/* <div className='h-full'>
+<div className='hidden md:flex h-full w-[72] z-30 flex-col fixed inset-y-0'>
+</div>
+<main className='md:pl-[72] h-full'>
+    {children}
+</main>
+</div> */}
---
+++ components/modals/delete-message-modal.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from '@/components/ui/dialog';
+
+import { useModal } from '@/hooks/use-modal-store';
+import { Button } from '../ui/button';
+import { useState } from 'react';
+import axios from 'axios';
+import { useParams, useRouter } from 'next/navigation';
+import queryString from 'query-string';
+
+
+
+
+
+export const DeleteMessageModal = () =>{
+
+    const { isOpen, onClose, type, data } = useModal();
+
+    const router = useRouter();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { apiUrl,query } = data;
+
+    const isModalOpen = isOpen && type === "deleteMessage";
+
+
+    const onClick = async () => {
+        try{
+            setIsLoading(true);
+            
+            const url = queryString.stringifyUrl({
+                url: apiUrl || "",
+                query,
+            })
+            await axios.delete(url);
+            onClose();
+        }catch(err){
+            console.log(err)
+        }finally {
+            setIsLoading(false);
+        }
+    }
+
+
+
+    return (
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+                <DialogHeader className='pt-8 px-6'>
+                    <DialogTitle className='text-2xl text-center'>
+                        Delete Message
+                    </DialogTitle>
+                    <DialogDescription className='text-center text-zinc-500'>
+                        Are you sure you want to do this? 
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter className='bg-gray-100 px-6 py-4'>
+                    <div className='flex items-center justify-between w-full'>
+                        <Button
+                            disabled={isLoading}
+                            onClick={onClose}
+                            variant="ghost"
+                        >
+                            Cancel
+                        </Button>
+                        <Button 
+                            disabled={isLoading}
+                            onClick={onClick}
+                            variant="primary"
+                        >
+                            Confirm
+                        </Button>
+                    </div>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
